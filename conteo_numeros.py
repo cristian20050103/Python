ca153a606af05ae9eb63d25e1701e26f48255dd6
Cristian Stiven Ruiz Rodriguez <125284840+cristian20050103@users.noreply.github.com>
2024-03-21 20:16:06 -0500

    Delete conteo_numeros.py

--- conteo_numeros.py
+++
@@ -1,5 +0,0 @@
-contador = 0
-numero=int(input("ingrese el numero"))
-for i in range(1, numero,2):
-    contador += 1 #con 1 cuenta los valores y con i los suma es el mismo codigo solo que cambia la variable de contador a suma  
-print("Cantidad de números del 1 al",numero,"de dos en dos:",contador)
