577f34ef1913a597e0d1a520a3469d8485a0860b
Cristian Stiven Ruiz Rodriguez <125284840+cristian20050103@users.noreply.github.com>
2024-03-21 20:16:37 -0500

    Delete suma_numeros.py

--- suma_numeros.py
+++
@@ -1,6 +0,0 @@
-numero=int(input("ingrese el numero"))
-suma=0
-for i in range(0,numero):
-    suma += i #con i suma los valores y con 1 los cuenta es el mismo codigo solo que cambia la variable de suma a contador
-    print("la suma de los numeros de 1 a",numero,"es:",suma)
-    
